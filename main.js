cab6af2dbed42bfc7f78766ccb4b74c7f8530493
salimboughanmi <boughanmi1salim@gmail.com>
2024-06-12 22:51:42 +0100

    read product

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ let taxes = document.getElementById('taxes');
 let ads = document.getElementById('ads');
 let discount = document.getElementById('discount');
 let total = document.getElementById('total');
-let count = document.getElementById('count');
 let category = document.getElementById('category');
 let submit = document.getElementById('submit');
 let search = document.getElementById('search');
@@ -45,6 +44,8 @@ submit.onclick = function(){
         console.log(dataProduct)
         localStorage.setItem('product',JSON.stringify(dataProduct))
         clearInputs()
+        showData()
+
 }
  
 // clear inputs
@@ -60,8 +61,27 @@ function clearInputs() {
     category.value = ''
 }
 
+// read product
 
+function showData() {
+let table =''
+for (let i = 0; i < dataProduct.length; i++) {
+  table += `<tr>
+              <td>${i}</td>
+              <td>${dataProduct[i].title}</td>
+              <td>${dataProduct[i].price}</td>
+              <td>${dataProduct[i].taxes}</td>
+              <td>${dataProduct[i].ads}</td>
+              <td>${dataProduct[i].discount}</td>
+              <td>${dataProduct[i].total}</td>
+              <td>${dataProduct[i].category}</td>
+              <td><button id="update">update</button></td>
+              <td><button id="delete">delete</button></td>
+            </tr>`
 
+        }
+        document.getElementById('tbody').innerHTML = table;
 
+    }
 
-
+       
